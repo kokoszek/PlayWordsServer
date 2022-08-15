6d1bb1c82a8b8379857fa0ba8622fda6b4d4ced8
Michael Frysztacki <frysztacki.tech@protonmail.com>
2022-08-15 20:50:17 +0200

    working socket.io !!!!

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 
+import { IoAdapter } from '@nestjs/platform-socket.io';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new WsAdapter(app));
+  app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(3000);
 }
 bootstrap();
--- src/websocket/ws-gateway.ts
+++ src/websocket/ws-gateway.ts
@@ -6,17 +6,23 @@ import {
 } from '@nestjs/websockets';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Server } from 'ws';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway(8080)
 export default class WsGateway {
   @WebSocketServer()
   server: Server;
 
-  @SubscribeMessage('events')
-  onEvent(client: any, data: any): Observable<WsResponse<number>> {
+  @SubscribeMessage('event')
+  onEvent(client: Socket, data: any): WsResponse<number> {
     console.log('server ws event reached');
-    //return 10;
-    return from([1]).pipe(map(item => ({ event: 'events', data: item })));
+    setTimeout(() => {
+      this.server.sockets.emit('msgToClient', { id: 40 });
+    }, 3000);
+    return {
+      event: '',
+      data: 2
+    };
+    //return from([1]).pipe(map(item => ({ event: 'events', data: item })));
   }
 }
