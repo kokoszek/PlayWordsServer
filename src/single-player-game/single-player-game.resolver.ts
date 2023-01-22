88c96f86360f71e699ce3fcba922eb20064ef932
Michael Frysztacki <michael.frysztacki@monterail.com>
2023-01-22 01:09:09 +0100

    WIP

--- src/single-player-game/single-player-game.resolver.ts
+++ src/single-player-game/single-player-game.resolver.ts
@@ -1,7 +1,8 @@
-import { Args, Query, Resolver } from "@nestjs/graphql";
+import { Args, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { TaskType } from "./task.type";
 import GameService from "../game/game-service";
 import WordConverter from "../word/word.converter";
+import { GraphQLBoolean, GraphQLInt } from "graphql";
 
 @Resolver(of => TaskType)
 export class SinglePlayerGameResolver {
@@ -17,12 +18,30 @@ export class SinglePlayerGameResolver {
   async generateTask(
     //@Args("level", { type: () => String }) lang: string
   ): Promise<TaskType> {
-    let task = await this.gameService.generateTask2("A1");
+    let task = await this.gameService.generateTask2("C2");
     console.log("--------", task);
     return {
       ...task,
       correctWord: task.correctWord,
       wordOptions: task.wordOptions
     };
+  }
+
+  /**
+   *
+   mutation SubmitTask($playerId: Int!, $meaningId: Int!, $wordIdSolution: Int!) {
+        submitTask(playerId: $playerId, meaningId: $meaningId, wordIdSolution: $wordIdSolution)
+    }
+   */
+  @Mutation(returns => GraphQLBoolean)
+  async submitTask(
+    @Args({ name: "playerId", type: () => GraphQLInt }) playerId: number,
+    @Args({ name: "meaningId", type: () => GraphQLInt }) meaningId: number,
+    @Args({ name: "wordIdSolution", type: () => GraphQLInt }) wordIdSolution: number
+  ): Promise<boolean> {
+    /**
+     * TODO: move nativeLang 'pl' to .env
+     */
+    return this.gameService.checkTaskSolution(meaningId, wordIdSolution, "pl");
   }
 }
